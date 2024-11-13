0522578fa5521bd5a01b3fed0f186ac98a6239b5
adrianalite <75641078+adrianalite@users.noreply.github.com>
2024-11-13 16:55:26 -0300

    Update lista9.py

--- lista9.py
+++ lista9.py
@@ -1,18 +1,20 @@
 import streamlit as st
 import pandas as pd
 
-df = pd.DataFrame({
-    'nomeServidor': ['Adriana', 'Monica', 'Samara'],
-    'salario': [1200,300,5000]
-})
+st.title('Localização das comunidades quilombolas (2022)')
+df = pd.read_csv('https://raw.githubusercontent.com/adrianalite/datasets/main/BR_LQs_CD2022.csv')
 
-st.write("Criando uma tabela!")
-st.write(df)
-opcao = st.selectbox(
-    'Qual servidor você gostaria de selecionar?',
-     df['nomeServidor'])
+df.fillna(0, inplace=True)
+df.drop(columns=['Unnamed: 0'], inplace=True)
+list = ['Lat_d', 'Long_d']
+# convertendo para numeros
+df[list] = df[list].apply(pd.to_numeric, errors='coerce')
+estados = df['NM_UF'].unique()
+estadoFiltro = st.selectbox(
+    'Qual estado selecionar?',
+     estados)
+dadosFiltrados = df[df['NM_UF'] == estadoFiltro]
+if st.checkbox('Mostrar tabela'):
+  st.write(dadosFiltrados)
+st.map(dadosFiltrados, latitude="Lat_d", longitude="Long_d")
 
-st.write('Você selecionou: ', opcao)
-
-dadosFiltrados = df[df['nomeServidor'] == opcao]
-dadosFiltrados
