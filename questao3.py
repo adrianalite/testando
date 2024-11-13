e22f38c632ac4b69bf09eb11fae24909bbd88bec
adrianalite <75641078+adrianalite@users.noreply.github.com>
2024-11-13 11:52:16 -0300

    Update questao3.py

--- questao3.py
+++ questao3.py
@@ -1,4 +1,26 @@
+#criando uma aplicação simples
 import streamlit as st
+#import no requirements.txt
+import pandas as pd
 
+df = pd.DataFrame({
+    'nomeServidor': ['Adriana', 'Monica', 'Samara'],
+    'salario': [1200,300,5000]
+})
+#posso trocar estes dados por dados de tabelas reais!
 
-st.select_slider("Grau de Satisfação", [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100])
+st.write("Criando uma tabela!")
+#tabelas interativas
+st.write(df)
+#inserindo um selectbox
+opcao = st.selectbox(
+    'Qual servidor você gostaria de selecionar?',
+     df['nomeServidor'])
+#O formato de print é diferente de outras versões
+#de Python
+st.write('Você selecionou: ', opcao)
+#como filtrar os dados pelo nome?
+
+#filtrando os dados pelo nome
+dadosFiltrados = df[df['nomeServidor'] == opcao]
+dadosFiltrados
