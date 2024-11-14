3f5ec7bd47a8f1e29e081e78a86bb716c577cc85
adrianalite <75641078+adrianalite@users.noreply.github.com>
2024-11-14 11:53:36 -0300

    Update questa2-lista9.py

--- questa2-lista9.py
+++ questa2-lista9.py
@@ -1,3 +1,9 @@
+import streamlit as st
+import pandas as pd
+
+st.title('Localizaçao das comunidades quilombolas (2022)')
+df = pd.read_csv('https://raw.githubusercontent.com/adrianalite/datasets/main/BR_LQs_CD2022.csv')
+
 #df.drop(columns=['Unnamed: 0'], inplace=True)
 list = ['Lat_d', 'Long_d']
 # convertendo para numeros
