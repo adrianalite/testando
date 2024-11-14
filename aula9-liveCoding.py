a07c4d37fe5cb5b5d70c2e465332ce3dbe4398f0
adrianalite <75641078+adrianalite@users.noreply.github.com>
2024-11-14 09:26:58 -0300

    Update aula9-liveCoding.py

--- aula9-liveCoding.py
+++ aula9-liveCoding.py
@@ -2,12 +2,14 @@ import streamlit as st
 import pandas as pd
 
 df = pd.DataFrame({
-    'nomeServidor':['Adriana', 'Thais', 'Samara'],
+    'nomeServidor':[ 'Adriana', 'Thais', 'Samara'],
     'salario':[10000,25000,20000]
 }   
 )
 st.write('Criando uma tabela')
 st.write(df)
 
-st.selectbox('Qual servidor você gostaria de selecionar?',
+opcao = st.selectbox('Qual servidor você gostaria de selecionar?',
              df['nomeServidor'])
+
+st.write(opcao)
